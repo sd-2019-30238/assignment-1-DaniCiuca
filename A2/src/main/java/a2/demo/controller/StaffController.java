bbb2b43d813a6ef63ede5e4181264d7ee9849e4d
Ciuca Daniel <dciuca98@gmail.com>
2019-05-16 02:44:13 +0300

    Staff(login,filters)

--- A2/src/main/java/a2/demo/controller/StaffController.java
+++ A2/src/main/java/a2/demo/controller/StaffController.java
@@ -1,13 +1,18 @@
 package a2.demo.controller;
 
+import a2.demo.model.Book;
 import a2.demo.model.Staff;
 import a2.demo.model.User;
+import a2.demo.repository.BookRepository;
 import a2.demo.repository.StaffRepository;
 import a2.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/rest/staff")
@@ -17,6 +22,105 @@ public class StaffController {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/login")
+    public ModelAndView displayLoginPage()
+    {
+        ModelAndView modelAndView = new ModelAndView("loginStaff");
+        modelAndView.addObject("staff",new Staff());
+        return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView verifyStaff(Staff staff)
+    {
+        ModelAndView modelAndView = new ModelAndView("loginStaff");
+        if(staffRepository.existsById(staff.getUsername())) {
+            if(staffRepository.findById(staff.getUsername()).get().getPassword().equals(staff.getPassword()))
+                return new ModelAndView("redirect:/rest/staff/home");
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/home")
+    public ModelAndView displayHomePage()
+    {
+        ModelAndView modelAndView = new ModelAndView("staffHome");
+        return modelAndView;
+    }
+
+    @PostMapping("/home")
+    public ModelAndView acceptUser(String username)
+    {
+        if(userRepository.existsById(username))
+        {
+            User u = userRepository.findById(username).get();
+            u.setAccepted(true);
+            userRepository.save(u);
+        }
+        ModelAndView modelAndView = new ModelAndView("staffHome");
+        return modelAndView;
+    }
+
+    @GetMapping("home/authors")
+    public ModelAndView filterAuthors()
+    {
+        ModelAndView modelAndView = new ModelAndView("viewAuthors");
+        return modelAndView;
+    }
+    @PostMapping("home/authors")
+    public ModelAndView filterAuthors1(String author1)
+    {
+        List<Book> books = bookRepository.findAll();
+        List<Book> books1= new ArrayList<Book>();
+        for(Book b : books)
+            if(b.getAuthor().toUpperCase().contains(author1.toUpperCase()))
+                books1.add(b);
+        ModelAndView modelAndView = new ModelAndView("viewAuthors");
+        modelAndView.addObject("authors",books1);
+        return modelAndView;
+    }
+
+    @GetMapping("home/titles")
+    public ModelAndView filterTitles()
+    {
+        ModelAndView modelAndView = new ModelAndView("viewTitles");
+        return modelAndView;
+    }
+    @PostMapping("home/titles")
+    public ModelAndView filterTitles1(String title1)
+    {
+        List<Book> books = bookRepository.findAll();
+        List<Book> books1= new ArrayList<Book>();
+        for(Book b : books)
+            if(b.getTitle().toUpperCase().contains(title1.toUpperCase()))
+                books1.add(b);
+        ModelAndView modelAndView = new ModelAndView("viewTitles");
+        modelAndView.addObject("titles",books1);
+        return modelAndView;
+    }
+
+    @GetMapping("home/genres")
+    public ModelAndView filterGenres()
+    {
+        ModelAndView modelAndView = new ModelAndView("viewGenres");
+        return modelAndView;
+    }
+    @PostMapping("home/genres")
+    public ModelAndView filterGenres1(String genre1)
+    {
+        List<Book> books = bookRepository.findAll();
+        List<Book> books1= new ArrayList<Book>();
+        for(Book b : books)
+            if(b.getGenre().toUpperCase().contains(genre1.toUpperCase()))
+                books1.add(b);
+        ModelAndView modelAndView = new ModelAndView("viewGenres");
+        modelAndView.addObject("genres",books1);
+        return modelAndView;
+    }
 
     @GetMapping
     public List<Staff> getAll() {
@@ -29,7 +133,7 @@ public class StaffController {
         return staffRepository.findAll();
     }
 
-    @GetMapping(value = "/{username}")
+    /*@GetMapping(value = "/{username}")
     public Staff findStaff(@PathVariable String username)
     {
         return staffRepository.findById(username).get();
@@ -47,4 +151,5 @@ public class StaffController {
         user.setAccepted(true);
         userRepository.save(user);
     }
+    */
 }
